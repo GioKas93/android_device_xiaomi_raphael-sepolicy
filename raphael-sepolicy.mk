bbddaef15ed725f338b2e83de3294e09c097fa8f
GioKas93 <giorgio_castelluccilibero.it>
2021-03-16 13:29:35 +0000

    raphael: add rules for vibration control

--- raphael-sepolicy.mk
+++ raphael-sepolicy.mk
@@ -33,6 +33,7 @@ BOARD_SEPOLICY_DIRS += \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/sensors \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/thermald \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/usb \
+	device/xiaomi/raphael-sepolicy/vendor/xiaomi/vibrator \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/wireless \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/wlan \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/hal_health_default.te
